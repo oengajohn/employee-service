1d6b6e5496a5a98a869d1e58c03aa02f71c34722
John Oenga Ongwae <johnongwae4@gmail.com>
2024-05-10 21:12:29 +0300

    Adjust image and refernces for secret information

--- src/main/java/io/github/oengajohn/employeeservice/controller/EmployeeController.java
+++ src/main/java/io/github/oengajohn/employeeservice/controller/EmployeeController.java
@@ -15,7 +15,6 @@ import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
