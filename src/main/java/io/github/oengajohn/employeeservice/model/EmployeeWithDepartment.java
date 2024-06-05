d6c1559fcfb83ac5d2731ade1f777808d33e16d9
John Oenga Ongwae <johnongwae4@gmail.com>
2024-06-05 20:17:45 +0300

    Upgrade dependencies

--- src/main/java/io/github/oengajohn/employeeservice/model/EmployeeWithDepartment.java
+++ src/main/java/io/github/oengajohn/employeeservice/model/EmployeeWithDepartment.java
@@ -3,9 +3,6 @@ package io.github.oengajohn.employeeservice.model;
 import java.time.LocalDate;
 
 import io.github.oengajohn.employeeservice.entity.Gender;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
